659d39ad65be1db74c2dd7bb37e2f83b8fb68f47
AlexMille <alx.mille@gmail.com>
2017-11-20 11:19:12 +0100

    Added gitignore

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 #Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+consumer_key = "Enter_your_consumer_key"
+consumer_secret = "Enter_your_consumer_secret"
+access_key = "Enter_your_access_key"
+access_secret = "Enter_your_access_secret"
